1df66ac1ca28bf9e266b1bb826440043618a919a
Pragathi-V <pragathivadigeri3@gmail.com>
2023-11-25 20:16:19 +0530

    commit

--- employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -2,10 +2,14 @@ package com.example.employeeservice.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.employeeservice.Dto.APIResponseDto;
 import com.example.employeeservice.Dto.EmployeeDto;
 import com.example.employeeservice.service.EmployeeService;
 
@@ -18,10 +22,17 @@ public class EmployeeController {
 	
 	private EmployeeService employeeService;
 	
+	@PostMapping
 	public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto) {
 		
 		EmployeeDto savedEmployee =  employeeService.saveEmployee(employeeDto);
 		return new ResponseEntity<EmployeeDto>(savedEmployee, HttpStatus.CREATED);
 	}
 
+	@GetMapping("{id}")
+	public ResponseEntity<APIResponseDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+		
+		APIResponseDto apiResponseDto = employeeService.getEmployeeById(employeeId);
+		return new ResponseEntity<APIResponseDto>(apiResponseDto, HttpStatus.OK);
+	}
 }
--- employee-service/src/main/java/com/example/employeeservice/entity/Employee.java
+++ employee-service/src/main/java/com/example/employeeservice/entity/Employee.java
@@ -28,5 +28,7 @@ public class Employee {
 	private String lastName;
 	@Column(nullable = false, unique = true)
 	private String email;
+	
+	private String departmentCode;
 
 }
